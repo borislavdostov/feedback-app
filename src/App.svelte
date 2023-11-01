499eb99d656d8110dacebd7052c3cf2049bdf92a
Borislav Dostov <borislav.dostov@kongsbergdigital.com>
2023-11-01 12:26:55 +0200

    Add Feedback stats

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import FeedbackList from "./components/FeedbackList.svelte";
+  import FeedbackStats from "./components/FeedbackStats.svelte";
 
   let feedback = [
     {
@@ -19,6 +20,11 @@
     },
   ];
 
+  $: count = feedback.length;
+  $: average =
+    feedback.reduce((prevItem, { rating }) => prevItem + rating, 0) /
+    feedback.length;
+
   const deleteFeedback = (e) => {
     const itemId = e.detail;
     feedback = feedback.filter((item) => item.id !== itemId);
@@ -26,5 +32,6 @@
 </script>
 
 <main class="container">
+  <FeedbackStats {count} {average} />
   <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
